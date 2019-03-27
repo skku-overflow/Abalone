ddb85bbde09330ac77c1d6129eada7304fd4e639
강동윤 <kdy1@outlook.kr>
2019-03-27 18:33:42 +0900

    흰 공 배치

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,15 +4,43 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Grid grid;
+    public Ball ballPrefab;
+
+
+
+    void Awake()
     {
-        
+        var whiteBalls = new Vector3Int[] {
+            new Vector3Int(-3, 1, 0),
+            new Vector3Int(-2, 1, 0),
+            new Vector3Int(-2, 0, 0),
+            new Vector3Int(-1, 1, 0),
+            new Vector3Int(-1, 0, 0),
+            new Vector3Int(0, 1, 0),
+            new Vector3Int(0, 0, 0),
+            new Vector3Int(1, 1, 0),
+            new Vector3Int(1, 0, 0),
+            new Vector3Int(2, 1, 0),
+            new Vector3Int(2, 0, 0),
+            new Vector3Int(-1, 2, 0),
+            new Vector3Int(0, 2, 0),
+            new Vector3Int(1, 2, 0),
+        };
+
+        foreach (Vector3Int pos in whiteBalls)
+        {
+            var loc = grid.CellToLocal(pos);
+            var ball = Instantiate<Ball>(ballPrefab);
+            ball.transform.SetParent(grid.transform);
+            ball.transform.localPosition = loc;
+        }
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
