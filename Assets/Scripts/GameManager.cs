8a2ec2e746f8233d6c0f2c5ab75d479fa8b06528
강동윤 <kdy1@outlook.kr>
2019-03-27 19:07:17 +0900

    클릭 가능

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,8 +6,12 @@ public class GameManager : MonoBehaviour
 {
     public Grid grid;
     public Ball ballPrefab;
+    public GameObject selectedPrefab;
 
 
+    private bool isBlackTurn = true;
+    private Vector3Int clicked = new Vector3Int(0, 0, -1);
+    private GameObject selected;
 
     void Awake()
     {
@@ -37,6 +41,7 @@ public class GameManager : MonoBehaviour
         }
 
     }
+
     private void RenderBall(Vector3Int pos, bool black)
     {
         var loc = grid.CellToLocal(pos);
@@ -47,12 +52,59 @@ public class GameManager : MonoBehaviour
         {
             ball.GetComponent<MeshRenderer>().material.color = Color.black;
         }
-
     }
+
+
 
     // Update is called once per frame
     void Update()
     {
+        if (selected != null)
+        {
+            selected.transform.Rotate(new Vector3(0, 0, 1));
+        }
 
+        if (Input.GetMouseButtonDown(0))
+        {
+            HandleClick();
+        }
+    }
+
+    private void HandleClick()
+    {
+        if (selected != null)
+        {
+            Destroy(selected);
+            selected = null;
+        }
+
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (!Physics.Raycast(ray, out hit)) return;
+
+        var cell = grid.WorldToCell(hit.transform.position);
+
+        if (clicked.z == -1)
+        {
+            clicked = cell;
+            selected = Instantiate(selectedPrefab);
+            var pos = grid.CellToLocal(cell);
+
+            selected.transform.SetParent(grid.transform);
+            pos.z = 1;
+            selected.transform.localPosition = pos;
+
+            return;
+        }
+
+        var distance = Vector3Int.Distance(clicked, cell);
+
+        // 거리가 1또는 sqrt(2) 인 경우
+        if (clicked != cell && distance <= 1.5f)
+        {
+            // 이동
+        }
+
+        clicked = new Vector3Int(0, 0, -1);
     }
 }
