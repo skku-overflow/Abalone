bda19847ca2efb07807921f7af5e3826c52056e6
강동윤 <kdy1@outlook.kr>
2019-04-04 21:11:21 +0900

    이동 및 공 밀기 구현 완료

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -9,6 +9,7 @@ public class GameManager : MonoBehaviour
     public Grid grid;
     public Ball ballPrefab;
     public GameObject selectedPrefab;
+    public GameObject pointPrefab;
 
     private Ball[,] balls;
     private bool isBlackTurn = true;
@@ -23,12 +24,9 @@ public class GameManager : MonoBehaviour
 
         // 흰 공 좌표. 
         var whiteBalls = new Vector3Int[] {
-            new Vector3Int(0, 1, 0),
             new Vector3Int(1, 1, 0),
-            new Vector3Int(1, 0, 0),
-            new Vector3Int(2, 0, 0),
             new Vector3Int(2, 1, 0),
-            new Vector3Int(2, 2, 0),
+            new Vector3Int(2, 0, 0),
             new Vector3Int(3, 0, 0),
             new Vector3Int(3, 1, 0),
             new Vector3Int(3, 2, 0),
@@ -37,22 +35,86 @@ public class GameManager : MonoBehaviour
             new Vector3Int(4, 2, 0),
             new Vector3Int(5, 0, 0),
             new Vector3Int(5, 1, 0),
+            new Vector3Int(5, 2, 0),
+            new Vector3Int(6, 0, 0),
+            new Vector3Int(6, 1, 0),
         };
 
         foreach (Vector3Int pos in whiteBalls)
         {
-            RenderBall(pos, false);
+            CreateBall(pos, false);
             var blackPos = pos;
             blackPos.y = 8 - blackPos.y;
-            RenderBall(blackPos, true);
+            CreateBall(blackPos, true);
+        }
+
+
+        var points = new Vector3Int[] {
+            new Vector3Int(0, 3, 0),
+            new Vector3Int(0, 4, 0),
+            new Vector3Int(1, 1, 0),
+            new Vector3Int(1, 2, 0),
+            new Vector3Int(1, 3, 0),
+            new Vector3Int(1, 4, 0),
+            new Vector3Int(2, 0, 0),
+            new Vector3Int(2, 1, 0),
+            new Vector3Int(2, 2, 0),
+            new Vector3Int(2, 3, 0),
+            new Vector3Int(2, 4, 0),
+            new Vector3Int(3, 0, 0),
+            new Vector3Int(3, 1, 0),
+            new Vector3Int(3, 2, 0),
+            new Vector3Int(3, 3, 0),
+            new Vector3Int(3, 4, 0),
+            new Vector3Int(4, 0, 0),
+            new Vector3Int(4, 1, 0),
+            new Vector3Int(4, 2, 0),
+            new Vector3Int(4, 2, 0),
+            new Vector3Int(4, 3, 0),
+            new Vector3Int(4, 4, 0),
+            new Vector3Int(5, 0, 0),
+            new Vector3Int(5, 1, 0),
+            new Vector3Int(5, 2, 0),
+            new Vector3Int(5, 3, 0),
+            new Vector3Int(5, 4, 0),
+            new Vector3Int(6, 0, 0),
+            new Vector3Int(6, 1, 0),
+            new Vector3Int(6, 2, 0),
+            new Vector3Int(6, 3, 0),
+            new Vector3Int(6, 4, 0),
+            new Vector3Int(7, 2, 0),
+            new Vector3Int(7, 3, 0),
+            new Vector3Int(7, 4, 0),
+            new Vector3Int(8, 4, 0),
+       };
+
+        foreach (Vector3Int pos in points)
+        {
+            CreatePoint(pos);
+            if (pos.y != 4)
+            {
+                var mirroredPos = pos;
+                mirroredPos.y = 8 - pos.y;
+                CreatePoint(mirroredPos);
+            }
         }
     }
 
-    /// <summary>HexGrid 상에 공을 생성하는 함수입니다.
+    private void CreatePoint(Vector3Int pos)
+    {
+        var point = Instantiate<GameObject>(pointPrefab);
+        point.transform.SetParent(grid.transform);
+        var loc = grid.CellToLocal(pos);
+        point.transform.localPosition = loc;
+        point.GetComponent<MeshRenderer>().material.color = Color.red;
+    }
+
+    /// <summary>
+    /// HexGrid 내부에 공을 생성합니다.
     /// </summary>
     /// <param name="pos"><c>Vector3Int</c> 형태의 좌표입니다.</param>
     /// <param name="black"><c>true</c>일 경우 검은 공으로, 아니면 하얀 공으로 초기화합니다.</param>
-    private void RenderBall(Vector3Int pos, bool black)
+    private void CreateBall(Vector3Int pos, bool black)
     {
         var ball = Instantiate<Ball>(ballPrefab);
         // 공을 grid의 child로 만듦
@@ -100,6 +162,7 @@ public class GameManager : MonoBehaviour
 
         // 클릭한 좌표 -> Hex 좌표
         var cell = grid.WorldToCell(hit.transform.position);
+        Debug.Log("선택: " + cell);
 
         // 만약 현재 클릭된 공이 없다면
         if (clicked.z == -1)
@@ -128,7 +191,6 @@ public class GameManager : MonoBehaviour
             pos.z = 1;
             selected.transform.localPosition = pos;
 
-            // Debug.Log("선택: " + cell);
 
             return;
         }
