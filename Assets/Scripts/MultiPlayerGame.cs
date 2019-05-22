ccc59f85f1893e0b8a550e040c2eab6519013b17
강동윤 <kdy1@outlook.kr>
2019-05-22 18:14:11 +0900

    firebase

--- Assets/Scripts/MultiPlayerGame.cs
+++ Assets/Scripts/MultiPlayerGame.cs
@@ -1,5 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using Firebase;
+using Firebase.Unity.Editor;
 using UnityEngine;
 
 public class MultiPlayerGame : MonoBehaviour
@@ -9,6 +11,8 @@ public class MultiPlayerGame : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        FirebaseApp.DefaultInstance.SetEditorDatabaseUrl("https://comit-abalone.firebaseio.com/");
+
 
     }
 
