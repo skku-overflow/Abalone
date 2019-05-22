658a05ec7f827ec9b32ec4c98c013649a3e2aa9a
강동윤 <kdy1@outlook.kr>
2019-05-22 18:20:34 +0900

    auth

--- Assets/Scripts/MultiPlayerGame.cs
+++ Assets/Scripts/MultiPlayerGame.cs
@@ -1,19 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using Firebase;
+using Firebase.Auth;
 using Firebase.Unity.Editor;
 using UnityEngine;
 
 public class MultiPlayerGame : MonoBehaviour
 {
     GameManager gameManager;
+    FirebaseAuth auth;
 
     // Start is called before the first frame update
     void Start()
     {
         FirebaseApp.DefaultInstance.SetEditorDatabaseUrl("https://comit-abalone.firebaseio.com/");
-
-
+        auth = FirebaseAuth.DefaultInstance;
     }
 
     // Update is called once per frame
