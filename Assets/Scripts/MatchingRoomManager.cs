da1240db329339b96eb31066c8ae1202456e272a
강동윤 <kdy1@outlook.kr>
2019-05-22 19:17:04 +0900

    Bypassed https://github.com/firebase/quickstart-unity/issues/106

---
+++ Assets/Scripts/MatchingRoomManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Firebase;
+using Firebase.Auth;
+using Firebase.Database;
+using Firebase.Unity.Editor;
+using UnityEngine;
+
+public class MatchingRoomManager : MonoBehaviour
+{
+    private FirebaseAuth auth;
+    private FirebaseDatabase db;
+    private DatabaseReference rooms;
+
+    void Start()
+    {
+        FirebaseApp.DefaultInstance.SetEditorDatabaseUrl("https://comit-abalone.firebaseio.com/");
+        auth = FirebaseAuth.DefaultInstance;
+        db = FirebaseDatabase.DefaultInstance;
+
+        rooms = db.GetReference("/matchings");
+
+        Debug.Log(rooms);
+    }
+
+    void Update()
+    {
+
+    }
+}
