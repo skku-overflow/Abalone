ab164a5ef361fb268e4f9bc4f44581a076de8e46
강동윤 <kdy1@outlook.kr>
2019-05-22 18:42:18 +0900

    firebase init

---
+++ Assets/Scripts/Login.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Firebase.Auth;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using Firebase;
+using Firebase.Unity.Editor;
+
+public class Login : MonoBehaviour
+{
+    public InputField email;
+    public InputField password;
+    public Button loginButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        FirebaseApp.DefaultInstance.SetEditorDatabaseUrl("https://comit-abalone.firebaseio.com/");
+        loginButton.onClick.AddListener(HandleLogin);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    async void HandleLogin()
+    {
+        try
+        {
+            var user = await FirebaseAuth.DefaultInstance.SignInWithEmailAndPasswordAsync(email.text, password.text);
+        }
+        catch (Exception e)
+        {
+            Debug.LogError(e);
+            var user = await FirebaseAuth.DefaultInstance.CreateUserWithEmailAndPasswordAsync(email.text, password.text);
+
+        }
+
+
+        SceneManager.LoadScene("SampleScene", LoadSceneMode.Additive);
+    }
+}
